97cac0371f017c3fc5c28397349cce957be42a5e
lemonsapp <lemonscontacto@gmail.com>
2026-03-16 02:50:08 +0000

    fix: unregister service worker on load

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
+// Limpiar Service Worker para evitar problemas de caché
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(r => r.unregister());
+  });
+  caches.keys().then(keys => {
+    keys.forEach(k => caches.delete(k));
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
